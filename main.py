411e2e2245bc69615e60977c4621520979667cac
Alfredo Boron <alron1984@gmail.com>
2024-10-09 23:34:41 -0700

    Added Circleshape OBJ, updated constants with Player class specific values, added Move/Rotate/Update methods to player class

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
 
@@ -12,13 +16,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
 
         screen.fill(BLACK)
+        player.draw(screen)
+        
         pygame.display.flip()
 
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+
+
+
 
 
 if __name__=="__main__":
